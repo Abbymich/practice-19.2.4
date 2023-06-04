3ea9511d7d3d5d97f3ea2a9c1abd2a1a135c93d8
Abbymich <eka.magomedkerimova@yahoo.com>
2023-06-05 00:10:58 +0500

    add files
    add files

---
+++ tests/test_calc.py
@@ -0,0 +1,22 @@
+import pytest
+
+from app.calculator import Calculator
+
+class TestCalc:
+    def setup(self):
+        self.calc = Calculator
+
+    def test_multiply_calculate_success(self):
+        assert self.calc.multiply(self, 2, 3) == 6
+
+    def test_division_calculate_success(self):
+        assert self.calc.division(self, 8, 2) == 4
+
+    def test_subtraction_calculate_success(self):
+        assert self.calc.subtraction(self, 7, 2) == 5
+
+    def test_adding_calculate_success(self):
+        assert self.calc.adding(self, 4, 2) == 6
+
+    def teardown(self):
+        print('Выполнение метода Teardown')
